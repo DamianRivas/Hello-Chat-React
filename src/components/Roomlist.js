0ff432376a85f27709baf4292df0f46d9a659822
DamianRivas <drivas1993@gmail.com>
2018-01-07 16:42:44 -0800

    Add user ability to create chatroom

--- src/components/Roomlist.js
+++ src/components/Roomlist.js
@@ -4,6 +4,7 @@ class Roomlist extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      newRoomName: '',
       rooms: []
     }
 
@@ -14,20 +15,39 @@ class Roomlist extends Component {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
-      console.log(room);
+      // console.log(room);
       this.setState({ rooms: this.state.rooms.concat(room) });
     });
   }
 
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    if(!this.state.newRoomName) { return }
+    this.roomsRef.push({
+      name: this.state.newRoomName
+    });
+    this.setState({ newRoomName: '' });
+  }
+
   render() {
     return (
-      <ul className="roomlist">
-        {
-          this.state.rooms.map( (room, index) =>
-            <li key={index}>{room.name}</li>
-          )
-        }
-      </ul>
+      <section className="sidebar">
+        <ul className="roomlist">
+          {
+            this.state.rooms.map((room, index) =>
+              <li key={index}>{room.name}</li>
+            )
+          }
+        </ul>
+        <form onSubmit={(e) => this.createRoom(e)}>
+          <input type="text" value={this.state.newRoomName} onChange={(e) => this.handleChange(e)}/>
+          <input type="submit" />
+        </form>
+      </section>
     );
   }
 
